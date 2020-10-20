15c0cf875d8bccc8fc652fa10c8b968557036b2b
jeongha-joo <64945491+jeongha-joo@users.noreply.github.com>
2020-10-20 17:23:18 +0900

    Add files via upload

---
+++ 과제/1019/Dlg_04.cpp
@@ -0,0 +1,270 @@
+﻿// Dlg_04.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include <time.h>
+#include "Dlg_04.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK    Rolling_Ball_Proc(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DLG04, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DLG04));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DLG04));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DLG04);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+
+BOOL first = TRUE;
+
+void RollingBall(HWND hWnd)
+{
+    static POINT pt = { 20, 20 };
+    static POINT oldpt = { 0,0 };
+    static RECT rect;
+    static int stepX = 10, stepY = 10;
+    
+    HDC hdc;
+    GetClientRect(hWnd, &rect);
+    hdc = GetDC(hWnd);
+    SetROP2(hdc, R2_XORPEN);
+    SelectObject(hdc, GetStockObject(WHITE_PEN));
+    SelectObject(hdc, GetStockObject(BLACK_BRUSH));
+    if (!first)
+        Ellipse(hdc, oldpt.x - 20, oldpt.y - 20, oldpt.x + 20, oldpt.y + 20);
+    else
+        first = FALSE;
+
+    ReleaseDC(hWnd, hdc);
+
+    pt.x = pt.x + stepX;
+    if (pt.x + 20 > rect.right || pt.x - 20 < 0) {
+        pt.x = pt.x - stepX;
+        stepX = -1 * stepX;
+    }
+
+    pt.y = pt.y + stepY;
+    if (pt.y + 20 > rect.bottom || pt.y - 20 < 0) {
+        pt.y = pt.y - stepY;
+        stepY = -1 * stepY;
+    }
+    Ellipse(hdc, oldpt.x - 20, oldpt.y - 20, oldpt.x + 20, oldpt.y + 20);
+    oldpt = pt;
+    ReleaseDC(hWnd, hdc);
+
+}
+
+BOOL CALLBACK Rolling_Ball_Proc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    HWND hButton;
+
+    switch (iMsg)
+    {
+    case WM_INITDIALOG:
+        hButton = GetDlgItem(hDlg, IDC_BUTTON_STOP);
+        EnableWindow(hButton, FALSE);
+        first = TRUE;
+        return 1;
+    case WM_TIMER:
+        RollingBall(hDlg);
+        break;
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case IDC_BUTTON_START:
+            
+            hButton = GetDlgItem(hDlg, IDC_BUTTON_START);
+            EnableWindow(hButton, FALSE);
+            hButton = GetDlgItem(hDlg, IDC_BUTTON_STOP);
+            EnableWindow(hButton, TRUE);
+            SetTimer(hDlg, 1, 50, NULL);
+            return 1;
+        case IDC_BUTTON_STOP:
+            hButton = GetDlgItem(hDlg, IDC_BUTTON_START);
+            EnableWindow(hButton, TRUE);
+            hButton = GetDlgItem(hDlg, IDC_BUTTON_STOP);
+            EnableWindow(hButton, FALSE);
+            KillTimer(hDlg, 1);
+            return 1;
+        case IDOK:
+            EndDialog(hDlg, 0);
+            return 0;
+        case IDCANCEL:
+            EndDialog(hDlg, 0);
+            return 0;
+        }
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case ID_DIALOG_OPEN:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ROLLINGBALL_DIALOG), hWnd, Rolling_Ball_Proc);
+                break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
